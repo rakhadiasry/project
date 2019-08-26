5f7b60a0b56b91721100d6686fd9b97a4c69f419
Rakha Diasry <rakha.diasry@gmail.com>
2019-08-26 11:22:28 +0700

    test

---
+++ application/views/dashboard.php
@@ -0,0 +1,18 @@
+<section class="content-header">
+  <h1>
+    Dashboard Page
+    <small>Control Panel</small>
+  </h1>
+  <ol class="breadcrumb">
+    <li><a href="#"><i class="fa fa-dashboard"></i></a></li>
+    <li class="active">Dashboard</li>
+</section>
+
+<!-- Main content -->
+<section class="content">
+
+  <!-- Default box -->
+
+  Dashboard page
+
+</section>
